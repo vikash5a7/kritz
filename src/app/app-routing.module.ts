678a410fb2326b9711d687362620ae6ca507e799
vikash5a7 <vikashkumargupta5a7@gmail.com>
2021-04-24 20:16:53 +0530

    fourth question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Com1Component } from './question/number-4/com1/com1.component';
+import { Com2Component } from './question/number-4/com2/com2.component';
+import { Com3Component } from './question/number-4/com3/com3.component';
+import { NavBarComponent } from './question/number-4/nav-bar/nav-bar.component';
 import { Number2Component } from './question/number2/number2.component';
 
 const routes: Routes = [
-  {path:"2",component:Number2Component},
+  { path: "2", component: Number2Component },
+  {
+    path: "nav", component: NavBarComponent,
+    children: [
+      { path: "first-comp", component: Com1Component },
+      { path: "second-comp", component: Com2Component },
+      { path: "third-comp", component: Com3Component },
+    ],
+  },
+
+  { path: '', redirectTo: '/nav', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { Number2Component } from './question/number2/number2.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { Com1Component } from './question/number-4/com1/com1.component';
+import { Com2Component } from './question/number-4/com2/com2.component';
+import { Com3Component } from './question/number-4/com3/com3.component';
+import { NavBarComponent } from './question/number-4/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Number2Component
+    Number2Component,
+    Com1Component,
+    Com2Component,
+    Com3Component,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
